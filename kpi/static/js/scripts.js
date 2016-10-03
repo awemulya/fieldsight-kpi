002ba9c3a483804434a32d4e01df9d38b6fae7a5
Niroj <nirojluitel@gmail.com>
2016-10-03 16:15:47 +0545

    favicon added,logo and right togglebar added

--- kpi/static/js/scripts.js
+++ kpi/static/js/scripts.js
@@ -8,10 +8,39 @@ $('#nav-accordion').dcAccordion({
                 autoExpand: true,
                 classExpand: 'dcjq-current-parent'
             });
+        $('.sidebar-toggle-box .fa-bars').click(function (e) {
+
+            $(".leftside-navigation").niceScroll({
+                cursorcolor: "#1FB5AD",
+                cursorborder: "0px solid #fff",
+                cursorborderradius: "0px",
+                cursorwidth: "3px"
+            });
+
+            $('#sidebar').toggleClass('hide-left-bar');
+            if ($('#sidebar').hasClass('hide-left-bar')) {
+                $(".leftside-navigation").getNiceScroll().hide();
+            }
+            $(".leftside-navigation").getNiceScroll().show();
+            $('#main-content').toggleClass('merge-left');
+            e.stopPropagation();
+            if ($('#container').hasClass('open-right-panel')) {
+                $('#container').removeClass('open-right-panel')
+            }
+            if ($('.right-sidebar').hasClass('open-right-bar')) {
+                $('.right-sidebar').removeClass('open-right-bar')
+            }
+
+            if ($('.header').hasClass('merge-header')) {
+                $('.header').removeClass('merge-header')
+            }
+
+
+        });
 (function ($) {
-    "use strict";
+    // "use strict";
     $(document).ready(function () {
-        /*==Left Navigation Accordion ==*/
+        
         
         /*==Slim Scroll ==*/
         if ($.fn.slimScroll) {
@@ -205,35 +234,7 @@ $('#nav-accordion').dcAccordion({
 
 
 
-        $('.sidebar-toggle-box .fa-bars').click(function (e) {
 
-            $(".leftside-navigation").niceScroll({
-                cursorcolor: "#1FB5AD",
-                cursorborder: "0px solid #fff",
-                cursorborderradius: "0px",
-                cursorwidth: "3px"
-            });
-
-            $('#sidebar').toggleClass('hide-left-bar');
-            if ($('#sidebar').hasClass('hide-left-bar')) {
-                $(".leftside-navigation").getNiceScroll().hide();
-            }
-            $(".leftside-navigation").getNiceScroll().show();
-            $('#main-content').toggleClass('merge-left');
-            e.stopPropagation();
-            if ($('#container').hasClass('open-right-panel')) {
-                $('#container').removeClass('open-right-panel')
-            }
-            if ($('.right-sidebar').hasClass('open-right-bar')) {
-                $('.right-sidebar').removeClass('open-right-bar')
-            }
-
-            if ($('.header').hasClass('merge-header')) {
-                $('.header').removeClass('merge-header')
-            }
-
-
-        });
         $('.toggle-right-box .fa-bars').click(function (e) {
             $('#container').toggleClass('open-right-panel');
             $('.right-sidebar').toggleClass('open-right-bar');
