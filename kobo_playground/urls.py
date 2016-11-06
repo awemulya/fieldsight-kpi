dda0e1f178d198523204115363cf1823dce43dc1
awemulya <awemulya@gmail.com>
2016-11-06 13:01:51 +0545

    admin sites disabled

--- kobo_playground/urls.py
+++ kobo_playground/urls.py
@@ -1,13 +1,13 @@
 from django.conf import settings
 from django.conf.urls import url, include
-from django.contrib import admin
+# from django.contrib import admin
 from django.views.generic.base import RedirectView
 
 
-admin.autodiscover()
+# admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
     url(r'^users/', include('users.urls', namespace='users')),
     url(r'^fieldsight/', include('fieldsight.urls', namespace='fieldsight')),
     # url(r'^', include('kpi.urls', namespace='kpi')),
