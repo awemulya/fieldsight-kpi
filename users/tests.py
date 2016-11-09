777a1912ce8a55dee60a43683618837292671fb7
awemulya <awemulya@gmail.com>
2016-11-09 12:00:59 +0545

    clean up

--- users/tests.py
+++ users/tests.py
@@ -4,35 +4,56 @@ from django.contrib.auth.models import User
 
 class LoginTestCase(TestCase):
     def setUp(self):
-        self.client = Client()
+        pass
 
-    def test_login_with_username(self):
-        user = User.objects.create_user('aaaaaa', 'aaaaaa@fs.com', '123456')
+    def _create_user(self, username, email, password):
+        user, created = User.objects.get_or_create(username=username)
+        user.set_password(password)
+        user.email = email
         user.is_active = True
         user.save()
-        response = self.client.post('/users/login/', {'username': 'aaaaaa', 'password': '123456'})
+
+        return user
+
+    def _login(self, username, email, password):
+        client = Client()
+        assert client.login(username=username, email=email, password=password)
+        return client
+
+    def _logout(self, client=None):
+        if not client:
+            client = self.client
+        client.logout()
+
+    def _create_user_and_login(self, username="bob", email="bob@fs.com", password="bob"):
+        self.login_username = username
+        self.login_password = password
+        self.user = self._create_user(username, email, password)
+        self.client = self._login(username, email, password)
+        self.anon = Client()
+
+    def test_login_with_username(self):
+        user = self._create_user('aaaaaa', 'aaaaaa@fs.com', '123456')
+        client = Client()
+        response = client.post('/users/login/', {'username': user.username, 'password': '123456'})
         self.assertEqual(response.templates[0].name, "registration/login.html")
 
     def test_bad_login(self):
         """Login Fails"""
-        response = self.client.post('/users/login/', {'email': 'john@fs.com', 'password': 'smith'})
+        client = Client()
+        response = client.post('/users/login/', {'email': 'john@fs.com', 'password': 'smith'})
         self.assertEqual(response.templates[0].name, "registration/login.html")
 
     def test_login(self):
         """Login Success"""
-        user = User.objects.create_user('john', 'lennon@thebeatles.com', '123456')
-        user.is_active = True
-        user.save()
-        response = self.client.post('/users/login/', {'email': user.email, 'password': '123456'})
-        self.assertEqual(response.content, "")
+        user = self._create_user('john', 'lennon@thebeatles.com', '123456')
+        client = self._login(user.username, user.email,"123456")
         self.client.get('/accounts/logout/')
 
     def test_koboform(self):
         "KOboform Page After Sucessfull Login "
-        user = User.objects.create_user('john', 'lennon@thebeatles.com', '123456')
-        user.is_active = True
-        user.save()
-        response = self.client.post('/users/login/', {'email': 'lennon@thebeatles.com', 'password': '123456'})
+        user = self._create_user('john', 'lennon@thebeatles.com', '123456')
+        response = self.client.post('/users/login/', {'email': user.email, 'password': '123456'})
         self.assertEqual(response.content, "")
         kobo = self.client.get('/')
         self.assertEqual(kobo.status_code, 200)
