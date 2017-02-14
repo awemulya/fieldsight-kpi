c9e419d3c618749ecf7b4094fca57a38e923a146
awemulya <awemulya@gmail.com>
2017-02-14 11:08:41 +0545

    get token from kpi removed

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^login/', views.web_login, name='web_login'),
-    url(r'^api/get-auth-token/$', views.ObtainAuthToken.as_view() ),
+    # url(r'^api/get-auth-token/$', views.ObtainAuthToken.as_view()),
     ]
 
